bc2069112cef6cb25146e853eeb10dd73137bc16
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-04-23 10:27:24 -0700

    removed cruft

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ApolloClient from 'apollo-boost';
-// import gql from 'graphql-tag';
-// import axios from 'axios';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';

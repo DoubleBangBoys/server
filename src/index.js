bd4bb8656e78a30c6757e1cbee839c0fe0e93d63
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-04-24 16:46:27 -0700

    comment out more code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router-3';
+// import { Router, Route, browserHistory, IndexRoute } from 'react-router-3';
 
 
 import App from './components/App';

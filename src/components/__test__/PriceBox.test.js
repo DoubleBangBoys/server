5f621a8aa56a1823c16e57ec38a7447a9742b291
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-04-24 13:43:27 -0700

    removed cruft

--- src/components/__test__/PriceBox.test.js
+++ src/components/__test__/PriceBox.test.js
@@ -8,7 +8,6 @@ import PriceBox from '../PriceBox';
 enzyme.configure({ adapter: new Adapter() });
 
 describe('<PriceBox />', () => {
-  // const wrapper = enzyme.mount(<PriceBox stringPrice="19.33" />);
   const component = enzyme.shallow(<PriceBox stringPrice="19.33" />);
   it('renders 1 <PriceBox /> component', () => {
     expect(component).toHaveLength(1);

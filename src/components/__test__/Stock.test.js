ee6f59986bcc10550817c77c0f32ac1e99711974
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-04-24 14:25:19 -0700

    Fixed Stock Test

--- src/components/__test__/Stock.test.js
+++ src/components/__test__/Stock.test.js
@@ -8,7 +8,7 @@ import Stock from '../Stock';
 enzyme.configure({ adapter: new Adapter() });
 
 describe('<Stock />', () => {
-  const component = enzyme.shallow(<Stock />);
+  const component = enzyme.shallow(<Stock renderInventory='<div id="inStock" className="colorGreen size-medium"> In Stock. </div>' />);
   it('renders 1 <Stock /> component', () => {
     expect(component).toHaveLength(1);
   });
